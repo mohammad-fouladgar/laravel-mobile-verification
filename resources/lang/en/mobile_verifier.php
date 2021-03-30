1b75ba3c6bedf9970c7a259799065a1073305272
balanso <lev1t@yandex.ru>
2021-03-30 16:41:43 +0300

    added not verified phone message

--- resources/lang/en/mobile_verifier.php
+++ resources/lang/en/mobile_verifier.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-
+    
     /*
     |--------------------------------------------------------------------------
     | Mobile Verifier Language file
@@ -14,5 +14,6 @@ return [
     'successful_verification' => 'Your mobile has been verified successfully.',
     'successful_resend'       => 'The token has been resent successfully.',
     'already_verified'        => 'Your mobile already has been verified.',
-    'expired_or_invalid' => 'The token has been expired or invalid.',
+    'expired_or_invalid'      => 'The token has been expired or invalid.',
+    'not_verified'            => 'Your mobile number is not verified.',
 ];
